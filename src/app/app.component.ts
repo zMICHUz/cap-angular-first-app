12dcb30a5eae9db9049f26a62945ef59f3680a0f
zMICHUz <miguelangelglezman@gmail.com>
2019-04-25 13:13:48 +0200

    :construction::hammer: inline resources extracted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'cap-app',
-  template: `<h1>Hello {{name}}!</h1>`
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
   name = 'Capgemini';
