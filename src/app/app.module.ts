2c3e1a0bd119e48995053dc392cdd5e92998e836
zMICHUz <miguelangelglezman@gmail.com>
2019-04-25 14:13:45 +0200

    :sparkles: new title component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, SharedModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
