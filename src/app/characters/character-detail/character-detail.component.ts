800881aca7fc6b740aeb71b7115c89b18d1a996d
zMICHUz <miguelangelglezman@gmail.com>
2019-04-25 14:52:54 +0200

    :construction: character detail logic

--- src/app/characters/character-detail/character-detail.component.ts
+++ src/app/characters/character-detail/character-detail.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import Character from '../../shared/character.model';
 
 @Component({
   selector: 'cap-character-detail',
   templateUrl: './character-detail.component.html',
   styleUrls: ['./character-detail.component.css']
 })
-export class CharacterDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class CharacterDetailComponent {
+  @Input() character: Character;
+} 
