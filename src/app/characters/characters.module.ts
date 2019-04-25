71a61d35445380cf45b3228eb5637bced496e820
zMICHUz <miguelangelglezman@gmail.com>
2019-04-25 14:30:26 +0200

    :sparkles::construction: characters list component scaffold

---
+++ src/app/characters/characters.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CharacterListComponent } from './character-list/character-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CharacterListComponent]
+})
+export class CharactersModule { }
