2e071f8815d3b38b27f043366b13069ad3c7fe40
zMICHUz <miguelangelglezman@gmail.com>
2019-04-25 14:52:21 +0200

    :sparkles::construction: characters detail component

---
+++ src/app/characters/character-detail/character-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cap-character-detail',
+  templateUrl: './character-detail.component.html',
+  styleUrls: ['./character-detail.component.css']
+})
+export class CharacterDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { CharacterListComponent } from './character-list/character-list.component';
+import { CharacterDetailComponent } from './character-detail/character-detail.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
-  declarations: [CharacterListComponent],
-  exports: [CharacterListComponent]
+  declarations: [CharacterListComponent, CharacterDetailComponent],
+  exports: [CharacterListComponent, CharacterDetailComponent]
 })
 export class CharactersModule { }
