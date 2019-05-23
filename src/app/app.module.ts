7ef035d9368d968be0113f0c1a4d60bd3a8319f9
zMICHUz <miguelangelglezman@gmail.com>
2019-05-23 22:49:24 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler, Injectable } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
@@ -6,8 +6,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { CharactersModule } from './characters/characters.module';
+import { LoggerService } from './core/logger.service';
 
 import { AppComponent } from './app.component';
+
+@Injectable()
+class MyErrorHandler implements ErrorHandler {
+  constructor(private logger: LoggerService) {}
+  
+  handleError(error) {
+    console.log('ERROR HANDLER!');
+    this.logger.log(error)
+  }
+}
 
 @NgModule({
   imports: [
@@ -19,6 +30,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule
   ],
   declarations: [ AppComponent ],
+  providers: [{provide: ErrorHandler, useClass: MyErrorHandler}],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
